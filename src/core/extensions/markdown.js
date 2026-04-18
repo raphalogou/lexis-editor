6b03c64e8b66f513ba1f5b0cf9932f08521d4597
Raphael Alogou <raphalogou@gmail.com>
2026-04-18 18:10:28 +0100

    feat: add Markdown divider support

--- src/core/extensions/markdown.js
+++ src/core/extensions/markdown.js
@@ -1,4 +1,9 @@
 import {
+  $createHorizontalRuleNode,
+  $isHorizontalRuleNode,
+  HorizontalRuleNode,
+} from "@lexical/extension";
+import {
   BOLD_STAR,
   CODE,
   HEADING,
@@ -11,12 +16,40 @@ import {
   STRIKETHROUGH,
   UNORDERED_LIST,
 } from "@lexical/markdown";
-import { defineExtension } from "lexical";
+import { $createParagraphNode, defineExtension } from "lexical";
 import { LexisExtension } from "./extension";
+
+const DIVIDER_REGEX = /^(?:---|\*\*\*|___)\s?$/;
+
+const DIVIDER = {
+  dependencies: [HorizontalRuleNode],
+  export: (node) => {
+    if (!$isHorizontalRuleNode(node)) {
+      return null;
+    }
+
+    return "---";
+  },
+  regExp: DIVIDER_REGEX,
+  replace: (parentNode, _children, _match, isImport) => {
+    const divider = $createHorizontalRuleNode();
+    parentNode.replace(divider);
+
+    if (isImport) {
+      return;
+    }
+
+    const paragraph = $createParagraphNode();
+    divider.insertAfter(paragraph);
+    paragraph.selectStart();
+  },
+  type: "element",
+};
 
 export const MARKDOWN_TRANSFORMERS = [
   HEADING,
   QUOTE,
+  DIVIDER,
   CODE,
   INLINE_CODE,
   UNORDERED_LIST,
